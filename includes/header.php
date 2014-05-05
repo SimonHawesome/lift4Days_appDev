8e152408db2e2b9dda48e42263fb9824ff5af9da
scrumy101 <sha@bsl.com>
2014-05-05 16:47:05 -0400

    end of day

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="styles.css" rel="stylesheet" type="text/css"/>
+<script src="scripts.js"></script>
 <title>Life 4 Days</title>
 </head>
 
--- public/log_workout.php
+++ public/log_workout.php
@@ -4,20 +4,14 @@
 <?php require_once("../includes/validation_functions.php"); ?>
 <?php require_once("../includes/header.php"); ?>
 
-
+<h3>Choose a muscle group</h3>
 <select name="exerciseChoice">
-  <option value="bicepCurls">Bicep Curls</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="legCurls">Leg Curls</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
-  <option value="tricepExtensions">Tricep Extensions</option>
+    <option value="none">- Select -</option>
+    <option value="arms">Arms</option>
+    <option value="chest">Chest</option>
+    <option value="back">Back</option>
+    <option value="legs">Legs</option>
+    <option value="shoulders">Shoulders</option>
 </select>
 
 
